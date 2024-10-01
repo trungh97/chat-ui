ced7db711fde8767a663988b0d879a961f78b9fa
trungh97 <hoangquoctrungit@gmail.com>
2024-10-01 20:31:43 +0700

    First graphql query

---
+++ app.js
@@ -0,0 +1,27 @@
+const GRAPHQL_URL = "http://localhost:9000/";
+
+async function fetchGreeting() {
+  const response = await fetch(GRAPHQL_URL, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          greeting
+        }
+      `,
+    }),
+  });
+
+  const data = await response.json();
+  
+  return data
+}
+
+fetchGreeting().then(({ data }) => {
+  const title = document.querySelector("h1");
+
+  title.textContent = data.greeting;
+});
