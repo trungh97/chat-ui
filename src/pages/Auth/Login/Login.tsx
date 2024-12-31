256ff7e266b80ba352704643f108c30e4b99f0c6
trungh97 <hoangquoctrungit@gmail.com>
2024-12-31 21:17:08 +0700

    Update login page styles

--- src/pages/Auth/Login/Login.tsx
+++ src/pages/Auth/Login/Login.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Button, GoogleSignInButton, Text } from 'slack-shared-ui'
+import { Button, Form, FormField, GoogleSignInButton } from 'slack-shared-ui'
 
 import { useAuth } from '@hooks/useAuth'
+import ChatIcon from '@assets/icons/ChatIcon'
 
 import { useGoogleLogin } from './hooks'
 
@@ -9,15 +10,51 @@ const Login = () => {
   const { data: authData, loading: authLoading } = useAuth()
   const { handleLogin } = useGoogleLogin()
 
+  const handleLoginCredentials = () => {
+    // TODO: Implement login with email and password
+  }
+
   return (
     <>
       {authLoading || (!authLoading && authData?.me.data?.id) ? (
         <div>Loading</div>
       ) : (
-        <div className="m-3 flex justify-center items-center gap-3 flex-col w-[360px] justify-self-center">
-          <Button className="w-full" label="Sign in" />
-          <GoogleSignInButton className="w-full" onClick={handleLogin} />
-          <Text />
+        <div className="m-3 flex justify-center items-center flex-col w-[360px] justify-self-center">
+          <ChatIcon width={48} height={48} />
+          <p className="text-gray-900 text-3xl font-semibold mt-6 mb-3">
+            Log in to your account
+          </p>
+          <span className="text-gray-600">
+            Welcome back! Please enter your details.
+          </span>
+
+          <Form
+            autoComplete="off"
+            className="w-full mt-8"
+            onSubmit={handleLoginCredentials}
+          >
+            <FormField
+              label="Email"
+              name="email"
+              placeholder="Email"
+              rules={[
+                { type: 'required', message: 'Email is required' },
+                { type: 'email', message: 'Invalid email' },
+              ]}
+              autoComplete="off"
+              className="m-0"
+            />
+            <FormField
+              label="Password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              rules={[{ type: 'required', message: 'Password is required' }]}
+              autoComplete="off"
+            />
+            <Button size="lg" label="Sign in" type="submit" />
+          </Form>
+          <GoogleSignInButton className="w-full mt-3" onClick={handleLogin} />
         </div>
       )}
     </>
