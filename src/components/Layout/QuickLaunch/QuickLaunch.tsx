7b00068e62d6ffc30493d656e568b21ce5837bd1
trungh97 <hoangquoctrungit@gmail.com>
2025-05-10 20:36:28 +0700

    Task #3: Fetch conversation list

--- src/components/Layout/QuickLaunch/QuickLaunch.tsx
+++ src/components/Layout/QuickLaunch/QuickLaunch.tsx
@@ -1,8 +1,31 @@
 import { MyAvatar } from '@components/UserAvatar/MyAvatar'
+import { MeDocument, MeQuery, useLogoutMutation } from '@generated/graphql'
 import React from 'react'
-import { NameAvatar } from 'shared-ui'
+import { Button, NameAvatar, Popover } from 'shared-ui'
 
 export const QuickLaunch = () => {
+  const [logout, { loading: logoutLoading }] = useLogoutMutation()
+
+  const onLogout = async () => {
+    await logout({
+      update(cache, { data }) {
+        if (data?.logout) {
+          cache.writeQuery<MeQuery>({
+            query: MeDocument,
+            data: {
+              me: {
+                statusCode: 401,
+                message: 'User is not authenticated!',
+                data: null,
+                error: 'User is not authenticated!',
+              },
+            },
+          })
+        }
+      },
+    })
+  }
+
   return (
     <div className="flex flex-col items-center justify-between px-1.5 py-12">
       <div className="flex flex-col items-center justify-center gap-y-4 px-4">
@@ -11,7 +34,22 @@ export const QuickLaunch = () => {
         <NameAvatar size="xl" firstName="John" lastName="Doe" />
       </div>
       <div>
-        <MyAvatar />
+        <Popover
+          content={
+            <Button
+              label="Logout"
+              intent='secondary'
+              onClick={onLogout}
+              disabled={logoutLoading}
+            />
+          }
+          placement="right"
+          trigger="click"
+        >
+          <div className='hover:cursor-pointer'>
+            <MyAvatar />
+          </div>
+        </Popover>
       </div>
     </div>
   )
