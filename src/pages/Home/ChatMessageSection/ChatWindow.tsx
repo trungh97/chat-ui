04637ed07494b7c9aab8bf7a199373baa531d530
trungh97 <hoangquoctrungit@gmail.com>
2025-08-07 16:43:56 +0700

    Task #10: Chat input UI

--- src/pages/Home/ChatMessageSection/ChatWindow.tsx
+++ src/pages/Home/ChatMessageSection/ChatWindow.tsx
@@ -1,12 +1,20 @@
 import React, { memo } from 'react'
+import { useParams } from 'react-router-dom'
 import { Divider } from 'shared-ui'
 import ChatHead from './ChatHead'
+import ChatInput from './ChatInput'
 import ChatMessageSection from './ChatMessageSection'
 import ChatMoreOptions from './ChatMoreOptions'
 import ChatTitle from './ChatTitle'
 import Wrapper from './Wrapper'
 
 const ChatWindow = memo(() => {
+  const { conversationId } = useParams()
+
+  if (!conversationId) {
+    return null
+  }
+
   return (
     <Wrapper>
       <ChatHead>
@@ -15,6 +23,7 @@ const ChatWindow = memo(() => {
       </ChatHead>
       <Divider className="my-3" color="#D5D7DA" />
       <ChatMessageSection />
+      <ChatInput />
     </Wrapper>
   )
 })
--- src/pages/Home/ChatMessageSection/index.ts
+++ src/pages/Home/ChatMessageSection/index.ts
@@ -1,1 +1,2 @@
 export * from './ChatMessageSection'
+export * from './ChatWindow'
